759aa06f260cafddf2e3bb8584f04ed7cdba8de7
christosgkoutzis <christosgkoutzis@gmail.com>
2024-01-15 18:18:31 +0200

    Updated main branch

--- db-connection.js
+++
@@ -1,6 +0,0 @@
-// Requires mongoose library
-const mongoose = require('mongoose');
-// Imports connection string from .env file
-const db = mongoose.connect(process.env.DB,{});
-
-module.exports = db;
