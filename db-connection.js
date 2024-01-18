6fc3df0afc8208ba8b8386d24a69cadbcdf5e1fa
christosgkoutzis <christosgkoutzis@gmail.com>
2024-01-18 15:16:16 +0200

    Deleted files from other branches

--- db-connection.js
+++
@@ -1,5 +0,0 @@
-// Requires mongoose dependency
-const mongoose = require('mongoose');
-// Connects to the DB through .env connection string
-const db = mongoose.connect(process.env.DB);
-module.exports = db;
